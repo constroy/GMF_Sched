ad09eddf0a16307d07c7ef32be3a4ad3c9800fbe
constroy <constroy@163.com>
2015-04-23 15:58:05 +0800

    completed the source code

--- src/Demo.c
+++ src/Demo.c
@@ -3,7 +3,17 @@
 
 void main()
 {
-  time_t timer,timerc;
-  int count=1;  
-  struct tm *timeinfo;
-  time(&timer);//绯荤粺寮
+	time_t timer,timerc;
+	int count=1;  
+	struct tm *timeinfo;
+	time(&timer);//系统开始的时间
+	while(1)
+	{
+		time(&timerc);
+		if((timerc-timer)>=1)//每过1秒打印
+		{
+			printf("程序经过%d秒\n",count++);
+			timer=timerc;
+		}
+	}
+}
